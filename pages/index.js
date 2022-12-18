6ceb7f003372676a97a41720cfe5db4adea8bbb2
Maxine Simpson <maxine.simpson@framecreates.co.uk>
2022-12-18 15:47:53 +0000

    test

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,9 @@ const Home = () => {
   const [winner, setWinner] = useState(people[0]);
   const [winnerSelected, setWinnerSelected] = useState(false);
   var intervalHandle = null;
+  console.log("v1.1");
+  console.log(people);
+
   const announceWinner = () => {
     clearInterval(intervalHandle);
     setTimeout(() => {
@@ -44,10 +47,10 @@ const Home = () => {
       <div className={`${styles.wrapper} ${isSpinning ? styles.spinning : styles.notSpinning}`}>
         {winnerSelected &&
           <Confetti
-          width={window.innerWidth}
-          height={window.innerHeight}
-          colors={['#FFFFFF', '#C0C0C0', '#fafafa87', '#fafafa54', '#FFFF00bf', '#ff460773']}
-        />
+            width={window.innerWidth}
+            height={window.innerHeight}
+            colors={['#FFFFFF', '#C0C0C0', '#fafafa87', '#fafafa54', '#FFFF00bf', '#ff460773']}
+          />
         }
         <div className={styles.wrapperInner}>
           {isSpinning && <h1 className={`${styles.homeHeader} ${winnerSelected ? styles.flash : styles.noFlash}`}>{winner.name}</h1>}
@@ -70,53 +73,52 @@ const initialPeopleList = [
   { name: 'Darren Healy', id: 9 },
   { name: 'David Calder', id: 11 },
   { name: 'Emma Lees', id: 13 },
-  { name: 'Ewan MacGill', id: 14},
-  { name: 'Florence Millar', id: 15},
-  { name: 'Gill Wylie', id: 16},
-  { name: 'Gordon Sheach', id: 17},
-  { name: 'Graeme Thewliss', id: 18},
-  { name: 'Graham Ferguson', id: 19},
-  { name: 'Hannah Wilson', id: 20},
-  { name: 'Heather Dewar', id: 22},
-  { name: 'James Henighen', id: 23},
-  { name: 'James Moran', id: 24},
-  { name: 'James Pais', id: 25},
-  { name: 'Jamie Ellis', id: 26},
-  { name: 'Jennifer McCall', id: 27},
-  { name: 'John Macfarlane', id: 28},
-  { name: 'Katie Brunsmann', id: 29},
-  { name: 'Katie Kelly', id: 30},
-  { name: 'Kevin Githwe', id: 32},
-  { name: 'Kevin Hickey', id: 33},
-  { name: 'Kieran Frew', id: 34},
-  { name: 'Laura Phillips', id: 35},
-  { name: 'Lauren Brogan', id: 36},
-  { name: 'Libby Keiller', id: 37},
-  { name: 'Mark Hume', id: 38},
-  { name: 'Mark McMullan', id: 39},
-  { name: 'Matthew Wiseman', id: 40},
-  { name: 'Maxine Simpson', id: 41},
-  { name: 'Niamh Coffey', id: 42},
-  { name: 'Nicola Devine', id: 43},
-  { name: 'Olivia Lynch', id: 44},
-  { name: 'Paddy Baxter', id: 45},
-  { name: 'Paul Fotheringham', id: 46},
-  { name: 'Peter Horne', id: 47},
-  { name: 'Peter McFarlane', id: 48},
-  { name: 'Raymond Rigby', id: 49},
-  { name: 'Roisin Tolmie', id: 50},
-  { name: 'Ruaridh Maclean', id: 51},
-  { name: 'Simon Parker', id: 52},
-  { name: 'Sophie Finch', id: 53},
-  { name: 'Stacey Lang', id: 54},
-  { name: 'Stephen Cooper', id: 55},
-  { name: 'Stuart Argue', id: 57},
-  { name: 'Suzanne Bennett', id: 58},
-  { name: 'Duncan Seller', id: 59},
-  { name: 'Stevie McNair', id: 60},
-  { name: 'Roslynn Mackay', id: 61},
-  { name: 'Gill Currie', id: 62},
-  { name: 'Tina Marchetti ', id: 63}
-
+  { name: 'Ewan MacGill', id: 14 },
+  { name: 'Florence Millar', id: 15 },
+  { name: 'Gill Wylie', id: 16 },
+  { name: 'Gordon Sheach', id: 17 },
+  { name: 'Graeme Thewliss', id: 18 },
+  { name: 'Graham Ferguson', id: 19 },
+  { name: 'Hannah Wilson', id: 20 },
+  { name: 'Heather Dewar', id: 22 },
+  { name: 'James Henighen', id: 23 },
+  { name: 'James Moran', id: 24 },
+  { name: 'James Pais', id: 25 },
+  { name: 'Jamie Ellis', id: 26 },
+  { name: 'Jennifer McCall', id: 27 },
+  { name: 'John Macfarlane', id: 28 },
+  { name: 'Katie Brunsmann', id: 29 },
+  { name: 'Katie Kelly', id: 30 },
+  { name: 'Kevin Githwe', id: 32 },
+  { name: 'Kevin Hickey', id: 33 },
+  { name: 'Kieran Frew', id: 34 },
+  { name: 'Laura Phillips', id: 35 },
+  { name: 'Lauren Brogan', id: 36 },
+  { name: 'Libby Keiller', id: 37 },
+  { name: 'Mark Hume', id: 38 },
+  { name: 'Mark McMullan', id: 39 },
+  { name: 'Matthew Wiseman', id: 40 },
+  { name: 'Maxine Simpson', id: 41 },
+  { name: 'Niamh Coffey', id: 42 },
+  { name: 'Nicola Devine', id: 43 },
+  { name: 'Olivia Lynch', id: 44 },
+  { name: 'Paddy Baxter', id: 45 },
+  { name: 'Paul Fotheringham', id: 46 },
+  { name: 'Peter Horne', id: 47 },
+  { name: 'Peter McFarlane', id: 48 },
+  { name: 'Raymond Rigby', id: 49 },
+  { name: 'Roisin Tolmie', id: 50 },
+  { name: 'Ruaridh Maclean', id: 51 },
+  { name: 'Simon Parker', id: 52 },
+  { name: 'Sophie Finch', id: 53 },
+  { name: 'Stacey Lang', id: 54 },
+  { name: 'Stephen Cooper', id: 55 },
+  { name: 'Stuart Argue', id: 57 },
+  { name: 'Suzanne Bennett', id: 58 },
+  { name: 'Duncan Seller', id: 59 },
+  { name: 'Stevie McNair', id: 60 },
+  { name: 'Roslynn Mackay', id: 61 },
+  { name: 'Gill Currie', id: 62 },
+  { name: 'Tina Marchetti ', id: 63 }
 ]
 export default Home;
